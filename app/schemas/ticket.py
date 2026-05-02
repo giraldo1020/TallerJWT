0860d2a1142f587f194e0bdb37518a12e89d7098
jdvelezgalgo <jdvelez@galgo.com>
2026-05-02 14:19:32 -0500

    agregar esquemas de ticke

---
+++ app/schemas/ticket.py
@@ -0,0 +1,34 @@
+# ==========================================
+# ESQUEMAS DE TICKET
+# ==========================================
+class TicketBase(BaseModel):
+    id_laboratorio: int
+    id_servicio: int
+    titulo: str
+    descripcion: str
+    prioridad: str  # Nota: idealmente esto sería un Enum (baja, media, alta), pero dejémoslo en string por simplicidad del taller.
+
+class TicketCreate(TicketBase):
+    # Hereda titulo, descripcion, etc. 
+    # NO incluimos id_solicitante, id_responsable, ni estado aquí.
+    pass
+
+class TicketEstadoUpdate(BaseModel):
+    # Este esquema es exclusivo para el PATCH /tickets/{id}/estado
+    nuevo_estado: str
+    observacion: Optional[str] = None # Opcional: por si el técnico o responsable quiere dejar un comentario al cambiar el estado
+
+class TicketResponse(TicketBase):
+    id_ticket: int
+    id_solicitante: int
+    id_responsable: Optional[int] = None
+    id_asignado: Optional[int] = None
+    estado: str
+    observacion_responsable: Optional[str] = None
+    observacion_tecnico: Optional[str] = None
+    fecha_creacion: datetime
+    fecha_actualizacion: datetime
+    fecha_finalizacion: Optional[datetime] = None
+
+    class Config:
+        from_attributes = True
